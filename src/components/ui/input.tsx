80205ef1eab7faf0513a1cd004c527593b9f43d6
ashiqul <ashiqul33@gmail.com>
2024-12-21 23:26:21 +0600

    fix type error

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,7 @@
 import { cn } from "src/utilities/cn";
 import { InputHTMLAttributes, forwardRef } from "react";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = forwardRef<HTMLInputElement, InputProps>(({ type, className, ...props }, ref) => {
