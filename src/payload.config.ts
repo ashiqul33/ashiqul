636789d196c8a661fc75028ba107bc56d1204b46
ashiqul <ashiqul33@gmail.com>
2024-12-22 00:38:47 +0600

    dang

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -16,7 +16,7 @@ import { Header } from "./Header/config";
 import { plugins } from "./plugins";
 import { defaultLexical } from "@/fields/defaultLexical";
 import { getServerSideURL } from "./utilities/getURL";
-import { Buffer } from "buffer";
+// import { Buffer } from "buffer";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
